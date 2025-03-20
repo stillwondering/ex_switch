4bde35367af2924d9decb1e158496ca69328f279
stillwondering <44878817+stillwondering@users.noreply.github.com>
2025-03-20 17:11:53 +0100

    Initial commit

---
+++ lib/ex_switch.ex
@@ -0,0 +1,18 @@
+defmodule ExSwitch do
+  @moduledoc """
+  Documentation for `ExSwitch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSwitch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_switch_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSwitchTest do
+  use ExUnit.Case
+  doctest ExSwitch
+
+  test "greets the world" do
+    assert ExSwitch.hello() == :world
+  end
+end
