eb54c79cf276372b8206c15aeb1fe51fedbdc9ef
stillwondering <44878817+stillwondering@users.noreply.github.com>
2025-03-21 06:53:20 +0100

    Remove unnecessary behaviour

--- lib/ex_switch/switch.ex
+++
@@ -1,7 +0,0 @@
-defmodule ExSwitch.Switch do
-  @type switch :: term()
-
-  @callback turn_on(switch()) :: {:ok, switch()} | {:error, term()}
-
-  @callback turn_off(switch()) :: {:ok, switch()} | {:error, term()}
-end
